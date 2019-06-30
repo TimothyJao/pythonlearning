ae5f95cbd9f54a169be74181a7248a311cfecf25
timothyjao <timothy.i.jao@gmail.com>
2019-06-30 14:54:09 -0700

    finish functions

---
+++ Functions/return.py
@@ -0,0 +1,11 @@
+# def print_square_of_7()
+#     print(7**2)
+
+# print_square_of_7()
+
+def square_of_7():
+    print("I AM BEFORE THE RETURN")
+    return(7**2)
+    print("I AM AFTER THE RETURN")
+
+print(square_of_7())
